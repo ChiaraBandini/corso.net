b1b020640255c252b7b1a7467dd7e1690638d27e
ChiaraBandini <chiara.bandini@yahoo.it>
2019-02-04 15:51:58 +0100

    code refactoring

---
+++ Esercizi.Net/Esercizi.net.BusinessLogic/ITransactionFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizi.net.BusinessLogic
+{
+
+    public interface ITransactionFactory : IFactory<ITransazione>
+    {
+    }
+
+}
